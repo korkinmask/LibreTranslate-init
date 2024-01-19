ee5182a9c16cb72d5dd8c799e429e52656a8178d
korkinmask <76049284+korkinmask@users.noreply.github.com>
2024-01-19 13:56:33 +0000

    One script

--- setup.sh
+++ setup.sh
@@ -25,3 +25,15 @@ virtualenv ~/LibreTranslate/env
 ~/LibreTranslate/env/bin/pip install ~/LibreTranslate/ --no-cache-dir
 ~/LibreTranslate/env/bin/libretranslate
 
+# Setup systemd
+sudo cp ~/LibreTranslate-init/libretranslate.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable libretranslate.service
+sudo systemctl start libretranslate.service
+# sudo systemctl status libretranslate
+
+# Configure Nginx
+sudo cp ~/LibreTranslate-init/nginx /etc/nginx/sites-available/default
+sudo nginx -t
+sudo systemctl restart nginx
+
